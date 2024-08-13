f59bfc021c1eee8833f64cb4e25ec51431c1fc0f
sayat <sayt9360@gmail.com>
2024-08-14 07:13:12 +0900

    1

--- library/sdxl_train_util.py
+++ library/sdxl_train_util.py
@@ -86,12 +86,12 @@ def _load_target_model(
         try:
             try:
                 pipe = StableDiffusionXLPipeline.from_pretrained(
-                    name_or_path, variant=variant, tokenizer=None,torch_dtype=model_dtype,device_map=str(device),
+                    name_or_path, variant=variant, tokenizer=None,torch_dtype=model_dtype,device_map="auto",
                 )
             except ValueError as ex:
                 if variant is not None:
                     logger.info("try to load fp32 model")
-                    pipe = StableDiffusionXLPipeline.from_pretrained(name_or_path, variant=None,torch_dtype=model_dtype,device_map=str(device), tokenizer=None)
+                    pipe = StableDiffusionXLPipeline.from_pretrained(name_or_path, variant=None,torch_dtype=model_dtype,device_map="auto", tokenizer=None)
                 else:
                     raise ex
         except EnvironmentError as ex:
