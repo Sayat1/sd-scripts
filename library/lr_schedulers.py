6507051b14c3a01b10e0bc6cdcea59a1a23b7de0
sayat <sayt9360@gmail.com>
2024-12-09 05:43:44 +0900

    1

--- library/lr_schedulers.py
+++ library/lr_schedulers.py
@@ -1,7 +1,7 @@
 from torch.optim.lr_scheduler import LambdaLR
 
 def lr_lambda_rex(
-        scheduler_steps: int,
+        total_steps: int,
 ):
     def lr_lambda(current_step: int):
         # https://arxiv.org/abs/2107.04197
@@ -9,8 +9,8 @@ def lr_lambda_rex(
         min_lr = 0
         d = 0.9
 
-        if current_step < scheduler_steps:
-            progress = (current_step / scheduler_steps)
+        if current_step < total_steps:
+            progress = (current_step / total_steps)
             div = (1 - d) + (d * (1 - progress))
             return min_lr + (max_lr - min_lr) * ((1 - progress) / div)
         else:
@@ -19,6 +19,6 @@ def lr_lambda_rex(
     return lr_lambda
 
 class REX(LambdaLR):
-    def __init__(self,optimizer,max_steps,last_epoch=-1):
-        super().__init__(optimizer, lr_lambda=lr_lambda_rex(max_steps), last_epoch=last_epoch)
+    def __init__(self,optimizer,total_steps,last_epoch=-1):
+        super().__init__(optimizer, lr_lambda=lr_lambda_rex(total_steps), last_epoch=last_epoch)
 
