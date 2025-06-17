d2b812e71f54cb9462c1c0d145bb8cb2a7a7c644
sayat <sayt9360@gmail.com>
2025-06-18 04:54:02 +0900

    1

--- train_network.py
+++ train_network.py
@@ -1163,7 +1163,7 @@ class NetworkTrainer:
                         loss = loss * weighting
 
                     if args.masked_loss or ("alpha_masks" in batch and batch["alpha_masks"] is not None):
-                        loss = apply_masked_loss(loss, batch, face_weight=args.masked_loss_face_weight, body_weight=args.masked_loss_body_weight, nonmask_weight=args.masked_loss_nonmask_weight)
+                        loss = apply_masked_loss(loss, batch, args)
                         
                     loss = loss.mean([1, 2, 3])
 
