73ae611f3f87291dde0014978dbd087fb7c73613
sayat <sayt9360@gmail.com>
2025-09-15 06:00:59 +0900

    1

--- library/custom_train_functions.py
+++ library/custom_train_functions.py
@@ -719,7 +719,7 @@ def get_mask_weight(target, batch, face_weight=1.0, body_weight=1.0):
 
     return merged_mask_image
 
-def apply_masked_latents(noisy_latents, noise, batch, vae_scale, args):
+def apply_masked_latents(noisy_latents, noise, batch, args):
     if "alpha_masks" in batch and batch["alpha_masks"] is not None:
         # alpha mask is 0 to 1
         mask_image = batch["alpha_masks"].to(dtype=noisy_latents.dtype).unsqueeze(1) # add channel dimension
@@ -729,9 +729,8 @@ def apply_masked_latents(noisy_latents, noise, batch, vae_scale, args):
 
     # resize to the same size as the loss
     mask_image = torch.nn.functional.interpolate(mask_image, size=noisy_latents.shape[2:], mode="area")
-    scaled_noise = noise * vae_scale
-    
-    noisy_latents = noisy_latents * mask_image + scaled_noise * (1 - mask_image)
+
+    noisy_latents = noisy_latents * mask_image + noise * (1 - mask_image)
 
     return noisy_latents
 
